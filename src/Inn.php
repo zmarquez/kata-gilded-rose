b0f3abc7f8de36436dd95a170f31e39006690243
Zacarías Márquez <zacarias.marquez@gmail.com>
2016-12-03 19:28:43 +0100

    Basic refactors

--- src/Inn.php
+++ src/Inn.php
@@ -3,54 +3,142 @@ namespace GildedRose;
 
 class Inn
 {
+    const MINIMUM_QUALITY = 0;
+    const MAXIMUM_QUALITY = 50;
+    const QUALITY_GRANULARITY = 1;
+    const SELLIN_GRANULARITY = 1;
+    const MINIMUM_SELLIN = 0;
+    const LIMIT_SELLIN_TWICE_FAST_QUALITY = 10;
+    const LIMIT_SELLIN_THRICE_FAST_QUALITY = 5;
+
     public function updateQuality($items)
     {
         foreach ($items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
+            if ($this->isAgedBrie($item)) {
+                $this->increaseQuality($item);
+                $this->decreaseSellin($item);
+                if ($this->isExpired($item)) {
+                    $this->increaseQuality($item);
                 }
             }
-
-            if ($item->name != 'Sulfuras') {
-                $item->sellIn = $item->sellIn - 1;
+            if ($this->isBackstagePass($item)) {
+                $this->increaseQuality($item);
+                $this->increaseExtraQuality($item);
+                $this->decreaseSellin($item);
+                if ($this->isExpired($item)) {
+                    $this->resetQuality($item);
+                }
             }
-
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+            if (!$this->isAgedBrie($item) && !$this->isBackstagePass($item)) {
+                $this->decreaseQuality($item);
+                if (!$this->isSulfuras($item)) {
+                    $this->decreaseSellin($item);
+                }
+                if ($this->isExpired($item)) {
+                    if (!$this->isSulfuras($item)) {
+                        $this->decreaseQuality($item);
                     }
                 }
             }
         }
+    }
+
+    /**
+     * @param Item $item
+     */
+    private function decreaseQuality(Item $item)
+    {
+        if ($item->quality <= self::MINIMUM_QUALITY) {
+            return;
+        }
+        if ($this->isSulfuras($item)) {
+            return;
+        }
+        $item->quality = $item->quality - self::QUALITY_GRANULARITY;
+
+    }
+
+    /**
+     * @param Item $item
+     */
+    private function increaseQuality(Item $item)
+    {
+        if ($item->quality >= self::MAXIMUM_QUALITY) {
+            return;
+        }
+        $item->quality = $item->quality + self::QUALITY_GRANULARITY;
+
+    }
+
+    /**
+     * @param Item $item
+     *
+     * @return bool
+     */
+    private function isAgedBrie(Item $item)
+    {
+        return $item->name == ItemType::AGED_BRIE;
+    }
+
+    /**
+     * @param Item $item
+     *
+     * @return bool
+     */
+    private function isBackstagePass(Item $item)
+    {
+        return $item->name == ItemType::BACKSTAGE_PASSES;
+    }
+
+    /**
+     * @param Item $item
+     *
+     * @return bool
+     */
+    private function isSulfuras(Item $item)
+    {
+        return $item->name == ItemType::SULFURAS;
+    }
+
+    /**
+     * @param $item
+     */
+    private function increaseExtraQuality($item)
+    {
+        if ($item->quality >= self::MAXIMUM_QUALITY) {
+            return;
+        }
+        if ($item->sellIn <= self::LIMIT_SELLIN_TWICE_FAST_QUALITY) {
+            $this->increaseQuality($item);
+        }
+        if ($item->sellIn <= self::LIMIT_SELLIN_THRICE_FAST_QUALITY) {
+            $this->increaseQuality($item);
+        }
+    }
+
+    /**
+     * @param $item
+     */
+    private function decreaseSellin($item)
+    {
+        $item->sellIn = $item->sellIn - self::SELLIN_GRANULARITY;
+    }
+
+    /**
+     * @param $item
+     */
+    private function resetQuality($item)
+    {
+        $item->quality = self::MINIMUM_QUALITY;
+    }
+
+    /**
+     * @param $item
+     *
+     * @return bool
+     */
+    private function isExpired($item)
+    {
+        return $item->sellIn < self::MINIMUM_SELLIN;
     }
 }
---
+++ src/ItemType.php
@@ -0,0 +1,10 @@
+<?php
+namespace GildedRose;
+
+class ItemType
+{
+    const DEFAULT_ITEM = 'Default';
+    const AGED_BRIE = 'Aged Brie';
+    const BACKSTAGE_PASSES = 'Backstage passes';
+    const SULFURAS = 'Sulfuras';
+}
